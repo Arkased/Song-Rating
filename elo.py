ffdcb12adb59ffd67ccaf4d1783a4799c204b650
Kevin Liu <kevintmliu@gmail.com>
2019-06-11 11:31:11 +0800

    changed expected value calculation to consider total power (Q_A and Q_B), renamed variables

--- elo.py
+++ elo.py
@@ -1,12 +1,26 @@
 # -*- coding: utf-8 -*-
 
-def _expected(A, B):
+_n = 400
+
+
+def _expected(a, b):
     """
-    Calculate expected score of A in a match against B
-    :param A: Elo rating for player A
-    :param B: Elo rating for player B
+    Calculate expected score of a in a match against b
+    :param a: Elo rating for player a
+    :param b: Elo rating for player b
     """
-    return 1 / (1 + 10 ** ((B - A) / 400))
+    q_a = _q(a)
+    q_b = _q(b)
+    return q_a / (q_a + q_b)
+
+
+def _q(a):
+    """
+    Calculates the relative expected score, which can be understood as absolute power.
+    :param a: Elo rating of some player
+    :return: q-value
+    """
+    return 10 ** (a / _n)
 
 
 def _elo(old, exp, score, k=32):
@@ -19,10 +33,20 @@ def _elo(old, exp, score, k=32):
     """
     return old + int(k * (score - exp))
 
+
 def update(song1, song2, score, k=32):
-    expA = _expected(song1.elo, song2.elo)
-    expB = 1 - expA
+    """
+    Updates the elos of song1 and song2 after match between song1 and song2
+    :param song1: first song
+    :param song2: second song
+    :param score: score of song1, 0 for loss 1 for win
+    :param k: weight of the match
+    :return: void
+    """
+    assert 0 <= score <= 1
+
+    exp_a = _expected(song1.elo, song2.elo)
+    exp_b = 1 - exp_a
     
-    song1.elo = _elo(song1.elo, expA, score, k)
-    song2.elo = _elo(song2.elo, expB, 1-score, k)
-    
+    song1.elo = _elo(song1.elo, exp_a, score, k)
+    song2.elo = _elo(song2.elo, exp_b, 1-score, k)
