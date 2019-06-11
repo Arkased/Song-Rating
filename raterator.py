ecff7f759130593e1ba27219fc24197d52a59835
Kevin Liu <kevintmliu@gmail.com>
2019-06-11 15:13:04 +0800

    improved post-match elo sorting (took advantage of fact that list is pre-sorted, minus possibly two songs which just changed elo)

--- raterator.py
+++ raterator.py
@@ -12,55 +12,24 @@ from elo import update
 
 _MIN_K = 20
 DEFAULT_FILE = 'songs.csv'
-COMPETE_RANGE = 10  # songs can compete against other songs within +-COMPETE_RANGE/2 places
+COMPETE_RANGE = 10  # all_songs can compete against other all_songs within +-COMPETE_RANGE/2 places
 
 
 def import_songs(file=DEFAULT_FILE):
-	global songs
+	global all_songs
 	with open(file, newline='\n', encoding='utf-8') as csvfile:
 		inp = list(reader(csvfile))
 
-	songs = [Song(row[0], row[1], int(row[2])) for row in inp]
+	all_songs = [Song(row[0], row[1], int(row[2])) for row in inp]
 
 
 def export_songs(file=DEFAULT_FILE, print_output=True):
-	song_data = [[song.name, song.artist, str(song.elo)] for song in songs]
+	song_data = [[song.name, song.artist, str(song.elo)] for song in all_songs]
 	if print_output:
 		print(song_data)
 	with open(file, 'w', newline='\n', encoding='utf-8') as f:
 		write = writer(f)
 		write.writerows(song_data)
-
-
-def raterate():
-	i1 = int(np.random.random() * num_songs)
-	i2 = i1 + int(((np.random.random() - .5) * COMPETE_RANGE))
-
-	if i1 != i2 and 0 <= i2 < num_songs:
-		compare(songs[i1], songs[i2])
-
-	raterate()
-
-
-def compare(song1: Song, song2: Song):
-	print(song1)
-	print('vs.')
-	print(song2)
-
-	update(song1, song2, _get_input(), _MIN_K)
-	print('Elo:', song1.elo, song2.elo)
-	print()
-	songs.sort()  # TODO: optimize
-
-
-def raterate_fixed(song1: Song):
-	i1 = songs.index(song1)
-	i2 = i1 + int(((np.random.random() - .5) * COMPETE_RANGE))
-
-	if i1 != i2 and 0 <= i2 < num_songs:
-		compare(song1, songs[i2])
-
-	raterate_fixed(song1)
 
 
 def _get_input():
@@ -79,6 +48,63 @@ def _get_input():
 		return _get_input()
 
 
+def compare(song1: Song, song2: Song):
+	print(song1)
+	print('vs.')
+	print(song2)
+
+	update(song1, song2, _get_input(), _MIN_K)
+	print('Elo:', song1.elo, song2.elo)
+	print()
+
+	all_songs.delete(song1)
+	all_songs.delete(song2)
+	insert([song1, song2])
+
+
+def insert(songs: list):
+	global all_songs
+	songs.sort()
+
+	list_a = all_songs[::-1]
+	list_b = songs[::-1]
+	new_list = []
+
+	while list_a and list_b:
+		if list_a[-1] < list_b[-1]:
+			new_list.append(list_a.pop())
+		else:
+			new_list.append(list_b.pop())
+
+	if list_a:
+		new_list.extend(list_a[::-1])
+	else:
+		new_list.extend(list_b[::-1])
+
+	all_songs = new_list
+
+
+def raterate():
+	i1 = int(np.random.random() * num_songs)
+	i2 = i1 + int(((np.random.random() - .5) * COMPETE_RANGE))
+
+	if i1 != i2 and 0 <= i2 < num_songs:
+		compare(all_songs[i1], all_songs[i2])
+
+	raterate()
+
+
+def raterate_fixed(song1: Song):
+	i1 = all_songs.index(song1)
+	i2 = i1 + int(((np.random.random() - .5) * COMPETE_RANGE))
+
+	if i1 != i2 and 0 <= i2 < num_songs:
+		compare(song1, all_songs[i2])
+
+	raterate_fixed(song1)
+
+
 import_songs()
-num_songs = len(songs)
-raterate()
+all_songs = [2, 4, 6, 8, 10]
+num_songs = len(all_songs)
+# raterate()
