bc07d7d4fca0e6c059a4c9ab2844111d94360bdc
Kevin Liu <kevintmliu@gmail.com>
2019-06-11 11:47:28 +0800

    cleaning, formatting, removed ghetto method to compare specific song, to be reimplemented (along with comparison within certain elo range and/or place ranking

--- raterator.py
+++ raterator.py
@@ -10,41 +10,32 @@ from song import Song
 import numpy as np
 from elo import update
 
-k = 22
+k = 22  # to be removed
 DEFAULT_FILE = 'songs.csv'
 COMPETE_RANGE = 10  # songs can compete against other songs within +-COMPETE_RANGE/2 places
 
 
 def import_songs(file=DEFAULT_FILE):
+    global songs
     with open(file, newline='\n', encoding='utf-8') as csvfile:
         inp = list(reader(csvfile))
 
-    return [Song(row[0], row[1], int(row[2])) for row in inp]
+    songs = [Song(row[0], row[1], int(row[2])) for row in inp]
 
 
-def export_songs(file=DEFAULT_FILE):
+def export_songs(file=DEFAULT_FILE, print_output=True):
     song_data = [[song.name, song.artist, str(song.elo)] for song in songs]
-    print(song_data)
+    if print_output:
+        print(song_data)
     with open(file, 'w', newline='\n', encoding='utf-8') as f:
         write = writer(f)
         write.writerows(song_data)
-    # np.savetxt(file, song_data, delimiter = ",", newline = '\n', encoding='utf-8')
-
-
-# songs = import_songs()
-num_songs = len(songs)
-
-
-# i1 = int(.8 * num_songs)
-# song1 = songs[i1]
 
 
 def raterate():
     inp = ''
     while inp != 's':
-        global song1
         i1 = int(np.random.random() * num_songs)
-        # i1 = songs.index(song1)
         i2 = i1 + int(((np.random.random() - .5) * COMPETE_RANGE))
         if i2 < 0:
             i2 = 0
@@ -53,36 +44,38 @@ def raterate():
         if i1 != i2:
             song1 = songs[i1]
             song2 = songs[i2]
+
             print(song1)
             print('vs.')
             print(song2)
-            update(song1, song2, get_input(), k)
+
+            update(song1, song2, _get_input(), k)
             print(song1.elo)
             print(song2.elo)
             songs.sort()
-        
 
-def get_input():
+
+def _get_input():
     inp = input()
-    if inp == 'k':
+    if inp == 'k':  # to be removed
         print('k=?')
         global k
         k = int(input())
-        return get_input()
+        return _get_input()
     if inp == 'e':
         export_songs()
-        raise Exception('exiting')
+        raise SystemExit
     if inp == '2':
         inp = '0'
-    inp = float(inp)
-    assert 0 <= inp <= 1
-    return inp
+    try:
+        score = float(inp)
+        assert 0 <= inp <= 1
+        return score
+    except ValueError or AssertionError:
+        print("invalid inp")
+        return _get_input()
 
 
-def clean(song):
-    song.name = song.name.replace(',', '')
-    song.artist = song.artist.replace(',', '')
-    return song
-
-
+import_songs()
+num_songs = len(songs)
 raterate()
